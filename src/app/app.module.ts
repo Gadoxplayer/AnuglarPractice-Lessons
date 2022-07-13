22083015c75fb79bd9ecd06f4980d3af44d70ed6
Jesus Campo Alvarez <jesus.campo@rockwellautomation.com>
2022-07-13 18:08:09 -0500

    building a structural directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 import { NumbersManagerComponent } from './numbers-manager/numbers-manager.component';
 import { BasicHighLightDirective } from './shared/basic-highlight.directive';
 import { BetterHighlightDirective } from './shared/better-highlight.directive';
+import { UnlessDirective } from './shared/unless.directive';
 
 @NgModule({
   declarations: [
@@ -17,7 +16,8 @@ import { BetterHighlightDirective } from './shared/better-highlight.directive';
     ServerElementComponent,
     NumbersManagerComponent,
     BasicHighLightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
