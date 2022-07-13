299a476334fd10168a710c0afe93b9f50fab911c
Jesus Campo Alvarez <jesus.campo@rockwellautomation.com>
2022-07-13 16:56:44 -0500

    creating a basic directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
 import { NumbersManagerComponent } from './numbers-manager/numbers-manager.component';
+import { BasicHighLightDirective } from './shared/basic-highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CockpitComponent,
     ServerElementComponent,
-    NumbersManagerComponent
+    NumbersManagerComponent,
+    BasicHighLightDirective
   ],
   imports: [
     BrowserModule,
